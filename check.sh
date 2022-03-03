8d74c47fc636e753f14b63201e8d2e3c2f74819d
kulaxa <mariojalso@gmail.com>
2022-03-03 16:12:46 +0100

    everything work, script is nice, code not so much :(

--- check.sh
+++ check.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
-
-#for test in /home/mario/Dropbox/Faks/UTR/Zadaće\ i\ labosi/lab1/testniPrimjer/*
-#do
- #   echo "$test"
-  #  java -jar "/home/mario/Dropbox/Faks/UTR/Zadaće i labosi/lab1/UTRlab1.jar" < "$test"
-   # diff rjesenja.txt ./testniRezultati/rez.txt
-#done
-
 for dire in ./testovi/*
 do
-	echo  $dire
-	#java -jar "/home/mario/Dropbox/Faks/UTR/Zadaće i labosi/lab1/UTRlab1.jar" < "$dire/test.a"
+	
 	java --class-path "/home/mario/Dropbox/Faks/UTR/Zadaće i labosi/UTRlab1/UTRlab1/src" lab1 < "$dire/test.a"
-	diff rjesenja.txt  "$dire/test.b"
+	difs="$(diff rjesenja.txt  "$dire/test.b")"
+		
+	if [ "$difs" = "" ];
+	then
+		echo "$dire : [OK]"
+	else
+		
+		echo "$dire : "
+		echo $difs
+	fi	
 
 done
 
